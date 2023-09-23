68a0189b3cd0c6ca0bd8a9dadbfd69b01695f86d
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2023-09-23 15:05:30 +0700

    Added: comments for view

--- querriesForView.py
+++ querriesForView.py
@@ -2,7 +2,39 @@ class ViewSelector:
     def __init__(self):
         columns = "PRODUCT PRODUCT_ID DOI A_PARAMETER_MAX A_PARAMETER_MIN MIXING_METHOD SOURCE_MIX_TIME_MIN SOURCE_MIX_TIME_MAX METHOD GAS SYNTHESIS_TIME_MIN SYNTHESIS_TIME_MAX FEATURE CONTRIBUTOR COMMENTS synthesis_parameters synthesis_units synthesis_min_values synthesis_max_values meas_parameters meas_units meas_mins meas_maxes All_ingredients words countries internal_cipher url YEAR journal impact"
         self.allColumns = columns.split()
-        print(self.allColumns)
+        self.allComments =  {
+"PRODUCT": "Формула продукта",
+"PRODUCT_ID": "Идентификатор продукта внутри базы",
+"DOI": "DOI",
+"A_PARAMETER_MAX": "Минимальное значение параметра А кубической фазы",
+"A_PARAMETER_MIN": "Максимальное значение параметра А кубической фазы",
+"MIXING_METHOD": "Метод смешивания",
+"SOURCE_MIX_TIME_MIN": "Минимальное значение времени смешивания сырья, часы",
+"SOURCE_MIX_TIME_MAX": "Максимальное значение времени смешивания сырья, часы",
+"METHOD": "Метод смешивания",
+"GAS": "Защитный газ",
+"SYNTHESIS_TIME_MIN": "Минимальное значение времени синтеза, минуты",
+"SYNTHESIS_TIME_MAX": "Максимальное значение времени синтеза, минуты",
+"FEATURE": "Признак работы",
+"CONTRIBUTOR": "ФИО вписавшего",
+"COMMENTS": "Комментарии",
+"synthesis_parameters": "Параметры синтеза",
+"synthesis_units": "Единицы измерения" ,
+"synthesis_min_values": "Минимальные значения параметров синтеза",
+"synthesis_max_values": "Максимальные значения параметров синтеза",
+"meas_parameters": "Параметры измерений",
+"meas_units": "Единицы измерений замеряемых параметров",
+"meas_mins": "Минимальные значения замеряемых параметров",
+"meas_maxes": "Максимальные значения замеряемых параметров",
+"All_ingredients": "Список ингредиентов",
+"words": "Ключевые слова",
+"countries": "Страны публикации",
+"internal_cipher": "Внутренний шифр",
+"url": "Ссылка",
+"YEAR": "Год выхода",
+"journal": "Журнал",
+"impact": "Импакт фактор"
+}
     def selectInfo(self, listOfColumns): # querry to get all columns in list
         if(len(listOfColumns)==0):
             listOfColumns = self.allColumns
@@ -18,13 +50,6 @@ class ViewSelector:
         return "SELECT table_comment     FROM INFORMATION_SCHEMA.TABLES     WHERE table_schema=\'heasm\'         AND table_name=\'MAIN_VIEW\';"
     def getAllColumns(self): # get list of all collumns
         return self.allColumns
-    def querryForColComments(self, column): # querry to get comments to the collumn
-        return "SELECT COLUMN_COMMENT FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = \'heasm\' AND TABLE_NAME = \'MAIN_VIEW\' AND COLUMN_NAME = \'"+column+"\';"
-
 
 VS = ViewSelector()
-print(VS.getAllColumns())
-print(VS.selectInfo(VS.getAllColumns()))
-print(VS.querryTableInfo())
-print(VS.querryForComments())
-print(VS.querryForColComments('PRODUCT'))
+print(VS.allComments)
