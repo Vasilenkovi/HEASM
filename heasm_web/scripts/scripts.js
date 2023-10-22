ae7c23f7512fc77f88bc4aa78852b6a8da3fcc30
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2023-10-22 13:09:06 +0700

    socketFix43

--- heasm_web/scripts/scripts.js
+++ heasm_web/scripts/scripts.js
@@ -1,5 +1,5 @@
- var socket = io('wss://heas.main.tpu.ru/data', {forceNew: true,
-        transports: ['websocket', 'pooling'], secure: true});
+ var socket = io('https://heas.main.tpu.ru/data', {forceNew: true,
+         secure: true});
         socket.on('dataChanged', (data) => {
             console.log(data['data'])
             var changes = document.querySelector('[class="userInput"]' + '[data-row="' + data['data'][0] + '"]' + '[data-cell="' + data['data'][1] + '"]')
