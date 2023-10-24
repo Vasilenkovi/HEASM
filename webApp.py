61c73d3c2a73cf735658bdc37fc35b1740ad4144
CatGamer7 <99312965+CatGamer7@users.noreply.github.com>
2023-10-24 19:17:16 +0700

    host & port changed

--- webApp.py
+++ webApp.py
@@ -265,4 +265,4 @@ def getNewID():
 if __name__ == "__main__": #If not executed as module
    #app.run(host="127.0.0.1", port=8080, debug=True) #Run app
 
-    socketio.run(app=app, host="127.0.0.1", port=8080, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app=app, host="0.0.0.0", port=443, debug=True, allow_unsafe_werkzeug=True)
